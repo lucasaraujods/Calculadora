59b0021577c4babb71a909051fcfa27e61ec7370
lucasaraujods <158510731+lucasaraujods@users.noreply.github.com>
2024-03-18 16:59:40 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -2,7 +2,38 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //ESCREVA SEU CÓDIGO AQUI!!
+function calculadora(numero1,numero2,operador)
+{
+    let resultado
+    let num1= parseInt(numero1)
+    let num2= parseInt(numero2)
+    
+    switch(operador)
+    {
+        case "+":
+            resultado = ('A soma dos dois números é igual a ' + (num1+num2))
+            break;
+        case "-":
+            resultado = ('A subtração dos dois números é igual a ' + (num1 - num2))
+            break;
+        case "*":
+            resultado = ('A multiplicação dos dois números é igual a ' + (num1 * num2))
+            break;
+        case "/":
+            resultado = ('A divisão dos dois números é igual a ' + (num1 / num2))
+            break;
+        case "e":
+            resultado = (' A exponenciação dos números é igual a ' + (num1 ** num2))
+            break;
+    }
 
+    if ((resultado == undefined) || (resultado > 1000000))
+    {
+        resultado= 'ERRO'
+    }
+
+    return resultado 
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
